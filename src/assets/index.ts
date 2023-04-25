1203fb3965eac3884ebd0a555c93fc4ab7d00d2c
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-04-25 12:46:53 +0400

    Added Styles Template

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import GroundBottom from './images/GroundBottom.png';
+import GroundTop from './images/GroundTop.png';
+import Logo from './images/Logo.png';
+
+export const images = {
+	GroundBottom,
+	GroundTop,
+	Logo,
+};
---
+++ src/styles.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+const breakpoints = [430, 1200, 1440, 1680];
+export const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+
+export const colors = {
+	back: '#10102F',
+	white: '#FFFFFF',
+	light: '#8888A6',
+	yellow: '#FCB223',
+};
+
+const GlobalStyle = createGlobalStyle`
+	*{
+		box-sizing: border-box;
+	}
+	html,body{
+		width: 100%;
+		height: 100%;
+	}
+	body{
+		font-family: 'Mulish', sans-serif;
+		background-color: ${colors.back};
+		color: ${colors.white};
+		margin: 0;
+		padding: 0;
+	}
+	h1, h2, h3, h4, h5, h6{
+		margin: 0;
+	}
+	h1{
+		font-weight: 800;
+		font-size: 96px;
+		line-height: 96;
+	}
+	h3{
+		font-weight: 800;
+		font-size: 46px;
+		line-height: 48;
+	}
+`;
+
+export default GlobalStyle;
